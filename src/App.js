356fe122a3e43c25bd3a0cf56dfe5d2209598c17
mwises <88902469+mwises@users.noreply.github.com>
2023-08-14 13:14:43 -0300

    lifecycle

--- src/App.js
+++ src/App.js
@@ -1,38 +1,73 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+const eventFn = () => {
+  console.log('h1 clicado');
+};
 
 function App() {
-  const [reverse, setReverse] = useState(false);
   const [counter, setCounter] = useState(0);
-  const reverseClass = reverse ? 'reverse' : '';
+  const [counter2, setCounter2] = useState(0);
 
-  const handleClick = () => {
-    setReverse(!reverse);
-  };
+  //componentDidUpdate - executa toda vez que o component atualiza
+  // useEffect(() => {
+  //   console.log('componentDidUpdate');
+  // });
 
-  const handleIncrement = () => {
-    setCounter(counter + 1);
-  };
+  //componentDidMount - executa 1x (UMA VEZ)
+  useEffect(() => {
+    document.querySelector('h1')?.addEventListener('click', eventFn);
+
+    // //componentWillUmont - limpeza
+    return () => {
+      document.querySelector('h1')?.removeEventListener('click', eventFn);
+    };
+  }, []);
+
+  //com dependencia - executa toda vez que a dependencia mudar
+  useEffect(() => {
+    console.log('c1:', counter, 'c2:', counter2);
+  }, [counter, counter2]);
+
+  // const [reverse, setReverse] = useState(false);
+  // const [counter, setCounter] = useState(0);
+  // const reverseClass = reverse ? 'reverse' : '';
+
+  // const handleClick = () => {
+  //   setReverse((reverse) => !reverse);
+  // };
+
+  // const handleIncrement = () => {
+  //   setCounter((counter) => counter + 1);
+  // };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
-
-        <h1>Contador: {counter}</h1>
-
-        <button type="button" onClick={handleClick}>
-          reverse {reverseClass}
-        </button>
-
-        <p>
-          <button type="button" onClick={handleIncrement}>
-            increment {counter}
-          </button>
-        </p>
-      </header>
+      <p>teste 2</p>
+      <h1>
+        c1: {counter} c2: {counter2}{' '}
+      </h1>
+      <button onClick={() => setCounter(counter + 1)}>+</button>
+      <button onClick={() => setCounter2(counter2 + 1)}>+ (2)</button>
     </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
+
+    //     <h1>Contador: {counter}</h1>
+
+    //     <button type="button" onClick={handleClick}>
+    //       reverse {reverseClass}
+    //     </button>
+
+    //     <p>
+    //       <button type="button" onClick={handleIncrement}>
+    //         increment {counter}
+    //       </button>
+    //     </p>
+    //   </header>
+    // </div>
   );
 }
 
