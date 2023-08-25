434a2c0e79c691b5d90305cb48916b1d7867e3b7
mwises <88902469+mwises@users.noreply.github.com>
2023-08-25 12:52:59 -0300

    Criação do meu próprio Hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
-import { AppContext } from './contexts/AppContext';
-import { H1 } from './components/H1';
+
+const useMyHook = (cb, delay = 1000) => {
+  const saveCb = useRef();
+  useEffect(() => {
+    saveCb.current = cb;
+  }, [cb]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      cb();
+    }, delay);
+
+    return () => clearInterval(interval);
+  }, [cb, delay]);
+};
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [delay, setDelay] = useState(1000);
+  const [incrementor, setIncrementor] = useState(100);
+
+  useMyHook(() => setCounter((c) => c + 1), delay);
+
   return (
-    <AppContext>
-      <div className="App">
-        <H1 />
-      </div>
-    </AppContext>
+    <div className="App">
+      <h1>contador: {counter}</h1>
+      <h1>delay: {delay}</h1>
+      <button
+        onClick={() => {
+          setDelay((d) => d + incrementor);
+        }}
+      >
+        +{incrementor}
+      </button>
+      <button
+        onClick={() => {
+          setDelay((d) => d - incrementor);
+        }}
+      >
+        -{incrementor}
+      </button>
+      <input type="number" value={incrementor} onChange={(e) => setIncrementor(Number(e.target.value))} />
+    </div>
   );
 }
 
